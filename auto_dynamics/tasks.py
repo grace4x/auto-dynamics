0476d2a872654dc5399c45dbb40c47f7bda3a883
grace4x <grace.rd.xu@gmail.com>
2024-09-01 18:23:11 -0700

    Update tasks.py

--- auto_dynamics/tasks.py
+++ auto_dynamics/tasks.py
@@ -6,17 +6,6 @@ from typing import Mapping
 import note_seq
 import jax
 
-
-# filename = '/home/grace/datasets/maestro/test/test.tfrecord'
-
-# raw_dataset = tf.data.TFRecordDataset('/home/grace/datasets/maestro/v3.0.0/maestro-v3.0.0_ns_wav_train.tfrecord-00000-of-00025')
-# for raw_record in raw_dataset.take(5):
-#     with tf.io.TFRecordWriter(filename) as writer:
-#         example = tf.train.Example()
-#         example.ParseFromString(raw_record.numpy())
-#         record_bytes = example.SerializeToString()
-#         writer.write(record_bytes)
-
 seqio.TaskRegistry.add(
     "midi_processing",
     source=seqio.TFExampleDataSource(
@@ -44,5 +33,3 @@ seqio.TaskRegistry.add(
             step_name='final'
         )
     ])
-
-# dataset = seqio.get_mixture_or_task("remove_wav").get_dataset()
